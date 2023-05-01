a6748abe0e18a426bdeeb9e70193e3307bb0ece5
Rahulalvk <rahulal432@gmail.com>
2023-05-01 01:03:44 -0700

    The powers of two

---
+++ Power/Power.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    internal class Power
+    {
+        public static void Displaypower()  
+        {
+            long pow;
+            Console.WriteLine("Enter the number upto which u want powers of 2");
+            int n = Convert.ToInt32(Console.ReadLine());    
+            for (int i = 1;i <= n; i++) 
+            {
+                pow = (long)Math.Pow(2, i);
+                Console.WriteLine(pow);
+            }
+        }
+    }
+}
